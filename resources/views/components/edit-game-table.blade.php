b8c7ad6de3eafd6fe6b1695003ab92d08e5f8561
Osbaldo Esquivel <osbaldo.esquivel@gmail.com>
2022-09-01 20:38:18 -0700

    Add pool button routing

--- resources/views/components/edit-game-table.blade.php
+++ resources/views/components/edit-game-table.blade.php
@@ -70,7 +70,7 @@
             </tr>
         @endforeach
         <tr class="bg-white flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-            <td colspan="3" class="text-right border-grey-light border hover:bg-gray-100 p-3">Total MNF score</td>
+            <td colspan="3" class="text-center sm:text-right border-grey-light border hover:bg-gray-100 p-3">Total MNF score</td>
             <td class="text-center border-grey-light border hover:bg-gray-100 p-3">
                 <div>
                     <form action="\submit-score" method="post">
@@ -84,9 +84,11 @@
             </td>
             <td class="text-center">
                 <div>
-                    <button type="submit" class="inline-flex items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
-                        Pool
-                    </button>
+                    <a href="{{ route('current-week') }}">
+                        <button class="inline-flex items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                            Pool
+                        </button>
+                    </a>
                 </div>
             </td>
         </tr>
